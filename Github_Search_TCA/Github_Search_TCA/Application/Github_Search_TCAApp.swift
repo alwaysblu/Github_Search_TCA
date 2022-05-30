252f2efa418e1f3a29d894478a7e4ecb82577a63
cjm <stepsw112@gmail.com>
2022-05-30 09:09:53 +0900

    ✨ [Feat] : 폴더 추가함

---
+++ Github_Search_TCA/Github_Search_TCA/Application/Github_Search_TCAApp.swift
@@ -0,0 +1,18 @@
+//
+//  Github_Search_TCAApp.swift
+//  Github_Search_TCA
+//
+//  Created by 최정민 on 2022/05/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@main
+struct Github_Search_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchView()
+        }
+    }
+}
---
+++ Github_Search_TCA/Github_Search_TCA/Presentation/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  onboardingApp
+//
+//  Created by 최정민 on 2022/05/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
