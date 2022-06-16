6f2c957f91a0d5be2fee843b74f3991d93474270
alwaysblu <stepsw112@gmail.com>
2022-06-16 09:54:23 +0900

    ✅ [Test] : AccessToken을 받는데 실패하는 경우 추가함

--- Github_Search_TCA/Github_Search_TCATests/TCA/SearchView/SearchViewReducerTests.swift
+++ Github_Search_TCA/Github_Search_TCATests/TCA/SearchView/SearchViewReducerTests.swift
@@ -22,9 +22,9 @@ class SearchViewReducerTests: XCTestCase {
                                                   cellEnvironment: mockSearchCellEnviroment
     )
     var nextAction: SearchAction!
+    state.code = "test" // github로 부터 code를 전달받아야 accessToken을 요청한다.
 
     //when
-    state.code = "test" // github로 부터 code를 전달받아야 accessToken을 요청한다.
     let cancellable = searchReducer(&state,
                                     .requestAccessToken,
                                     mockSearchEnvironment
@@ -43,6 +43,43 @@ class SearchViewReducerTests: XCTestCase {
 
     //then
     XCTAssertNotEqual(state.accessToken, .empty)
+    cancellable.cancel()
+  }
+
+  func test_requestAccessToken_whenAccessToken을실패하는경우_thenState의AccessToken이empty이다() {
+    //given
+    let expectation = expectation(description: "AccessToken Request Fail")
+    var state: SearchState = .empty
+    let mockGithubRepository = MockGithubRepository.fail
+    let mockSearchCellEnviroment = SearchCellEnvironment(githubRepository: mockGithubRepository,
+                                                         mainQueue: .main
+    )
+    let mockSearchEnvironment = SearchEnvironment(githubRepository: mockGithubRepository,
+                                                  mainQueue: .main,
+                                                  cellEnvironment: mockSearchCellEnviroment
+    )
+    var nextAction: SearchAction!
+    state.code = "test" // github로 부터 code를 전달받아야 accessToken을 요청한다.
+
+    //when
+    let cancellable = searchReducer(&state,
+                                    .requestAccessToken,
+                                    mockSearchEnvironment
+    ).sink(receiveCompletion: { _ in
+      expectation.fulfill()
+    }, receiveValue: { action in
+      XCTAssertEqual(action, .accessTokenResponse(.failure(TestError.network)))
+      nextAction = action
+    })
+
+    waitForExpectations(timeout: 5, handler: nil) // 스케줄링 고려해서 테스트하기
+
+    _ = searchReducer(&state,
+                      nextAction,
+                      mockSearchEnvironment)
+
+    //then
+    XCTAssertEqual(state.accessToken, .empty)
     cancellable.cancel()
   }
 }
